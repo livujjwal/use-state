6167f18ccc30109141efaf33eef4bbc204d68b2a
livujjwal <ujjwalsingh2112@gamil.com>
2023-10-21 16:32:43 +0530

    form

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import BasicForm from "./Components/BasicForm";
 import StatesWithObjects from "./Components/StatesWithObjects";
 import StatesWithArrays from "./Components/StatesWithArrays";
 import FormValidation from "./Components/FormValidation";
+import "./App.css"
 const App = () => {
     return (
         <div>
 {/* <States/> */}
 {/* <Counter/> */}
 {/* <BasicForm/> */}
-<StatesWithObjects/>
+{/* <StatesWithObjects/> */}
 {/* <StatesWithArrays/> */}
 <FormValidation/>
         </div>
--- src/Components/FormValidation.js
+++ src/Components/FormValidation.js
@@ -1,6 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormValidation = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  let { name, email, password, confirmPassword } = user;
+  function handleInput(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
 
-}
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  function getUserInfo(e) {
+    e.preventDefault();
+    if (!name || !email || !password || !confirmPassword) {
+      setError("All field requireds");
+      setSuccess("");
+    } else if (!name.includes(" ")) {
+      setError("Please enter your first name and last name");
+      setSuccess("");
+    } else if (!email.includes("@")) {
+      setError("Please enter valid email");
+      setSuccess("");
+    } else if (password !== confirmPassword) {
+      setError("Please enter same password");
+      setSuccess("");
+    } else {
+      setSuccess("Form Submited Successfully");
+      setError("");
+      setUser({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    }
+  }
+  return (
+    <div className="signup">
+      {error ? <h4>{error}</h4> : ""}
+      {success ? <h4>{success}</h4> : ""}
+      <form onSubmit={getUserInfo}>
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={name}
+          onChange={handleInput}
+          name="name"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleInput}
+          name="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleInput}
+          name="password"
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={handleInput}
+          name="confirmPassword"
+        />
+        <button> Submit </button>
+      </form>
+    </div>
+  );
+};
 export default FormValidation;
